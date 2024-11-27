bf49e93a4bdcad78f6beaa99502b14e09a781bc8
evilyn-cordeiro <evilyndeveloper@gmail.com>
2024-11-27 20:30:05 -0300

    portfolio v1.8

--- src/components/Certification/const.ts
+++ src/components/Certification/const.ts
@@ -1,24 +1,13 @@
 export const certifications = [
   {
-    title: "HTML5 e CSS3 Avançados",
-    school: "Udemy",
-    imgUrl: "https://www.langoly.com/wp-content/uploads/2022/09/udemy-logo.png",
-    credentialing: "",
-    skills: [
-      "Desenvolvimento de layouts responsivos (mobile e desktop)",
-      "Criação de interfaces profissionais, do protótipo até a implementação final",
-      "Aplicação de boas práticas de acessibilidade e SEO",
-    ],
-  },
-  {
     title: "JavaScript Avançado",
     school: "Udemy",
     imgUrl: "https://www.langoly.com/wp-content/uploads/2022/09/udemy-logo.png",
     credentialing: "",
     skills: [
-      "Desenvolvimento de interatividade com JavaScript (funcional e orientado a objetos)",
-      "Prototipagem de interfaces interativas de alta e baixa fidelidade",
-      "Integração com APIs e manipulação dinâmica de dados",
+      "Interatividade com JavaScript (funcional e OO)",
+      "Prototipagem de interfaces interativas",
+      "Integração com APIs e manipulação de dados",
     ],
   },
   {
@@ -27,21 +16,33 @@ export const certifications = [
     imgUrl: "https://s.zst.com.br/prod/cupons/19290-Logo-200x200.png",
     credentialing: "",
     skills: [
-      "Desenho e normalização de banco de dados (diagramas ER)",
-      "Integração de dados com interfaces e sistemas front-end",
-      "Aplicação de princípios de usabilidade no design de dados",
+      "Desenho e normalização de banco de dados",
+      "Integração de dados com front-end",
+      "Princípios de usabilidade em design de dados",
     ],
   },
   {
-    title: "Figma Design de Interfaces",
+    title: "TypeScript Avançado",
     school: "Udemy",
     imgUrl: "https://www.langoly.com/wp-content/uploads/2022/09/udemy-logo.png",
     credentialing:
       "https://www.udemy.com/certificate/UC-a48bfda0-617e-41d5-978a-8d7d72abca30/",
     skills: [
-      "Criação de guias de estilo completos (cores, componentes, tipografia)",
-      "Desenvolvimento de protótipos interativos de alta fidelidade",
-      "Colaboração e versionamento de design em equipe",
+      "Criação de aplicações com TypeScript",
+      "Integração de TypeScript com React",
+      "Uso de Generics do TypeScript",
+    ],
+  },
+  {
+    title: "React Avançado",
+    school: "Udemy",
+    imgUrl: "https://www.langoly.com/wp-content/uploads/2022/09/udemy-logo.png",
+    credentialing:
+      "https://www.udemy.com/certificate/UC-a48bfda0-617e-41d5-978a-8d7d72abca30/",
+    skills: [
+      "Desenvolvimento de aplicações completas com React",
+      "Integração de TypeScript e React",
+      "Uso avançado de Generics em TypeScript",
     ],
   },
 ];
